07b645943a717946cf4c5f6b3691ba49eab2ad85
knyatom <knyatom@daum.netm>
2021-09-01 01:54:20 +0900

    901

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Navbar,Container,Nav, NavDropdown,Button} from 'react-bootstrap';
+import React ,{useState} from 'react';
+import { Navbar,Container,Nav, NavDropdown,Button,Row,Col} from 'react-bootstrap';
 import './App.css';
-
+//import {name, name2} from './data.js';
+import Data from './data.js';
 
 function App() {
+
+  let [shoes, shoes변경]=useState(Data);
+
   return (
     <div>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home"> Show Shop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -25,8 +29,8 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>      
-      <section>
-        <h1>Hello, world!</h1>
+      <section className="background">
+        <h1>20% Season Off</h1>
         <p>
           This is a simple hero unit, a simple jumbotron-style component for calling
           extra attention to featured content or information.
@@ -35,7 +39,38 @@ function App() {
           <Button variant="primary">Learn more</Button>
         </p>
       </section>
-</div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="shoes1.jpg" width="100%"/>
+            <h4> {shoes[0].title}</h4>
+            <p>  {shoes[0].content} &{shoes[0].price}</p>
+          </div>
+          <div className="col-md-4">
+          <img src="shoes2.jpg" width="100%"/>
+            <h4>  {shoes[1].title}</h4>
+            <p>  {shoes[1].content} &{shoes[1].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="./shoes3.jpg" width="100%"/>
+              <h4>  {shoes[2].title}</h4>
+              <p>  {shoes[2].content} &{shoes[2].price}</p>
+          </div>
+        </div>
+      </div>
+      <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
+   </div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,29 @@
+//const name="KimNY";
+//const name2="YouNA";
+//export default name;
+//export {name, name2}
+
+// 페이지 마지막에 한번만 선언한다.
+
+export default [
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000
+    }
+  ] 
