ce219385b766357632a9bae5042adcbd0f5b864a
knyatom <knyatom@daum.netm>
2021-09-02 02:00:14 +0900

    902

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React ,{useState} from 'react';
-import { Navbar,Container,Nav, NavDropdown,Button,Row,Col} from 'react-bootstrap';
-import './App.css';
+import React, { useState } from "react";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Button,
+  Row,
+  Col,
+} from "react-bootstrap";
+import "./App.css";
 //import {name, name2} from './data.js';
-import Data from './data.js';
+import Data from "./data.js";
 
 function App() {
-
-  let [shoes, shoes변경]=useState(Data);
+  let [shoes, shoes변경] = useState(Data);
 
   return (
     <div>
@@ -20,20 +27,26 @@ function App() {
               <Nav.Link href="#link">Link</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
-      </Navbar>      
+      </Navbar>
       <section className="background">
         <h1>20% Season Off</h1>
         <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
         </p>
         <p>
           <Button variant="primary">Learn more</Button>
@@ -42,36 +55,42 @@ function App() {
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="shoes1.jpg" width="100%"/>
-            <h4> {shoes[0].title}</h4>
-            <p>  {shoes[0].content} &{shoes[0].price}</p>
-          </div>
-          <div className="col-md-4">
-          <img src="shoes2.jpg" width="100%"/>
-            <h4>  {shoes[1].title}</h4>
-            <p>  {shoes[1].content} &{shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="./shoes3.jpg" width="100%"/>
-              <h4>  {shoes[2].title}</h4>
-              <p>  {shoes[2].content} &{shoes[2].price}</p>
-          </div>
+            {/* <Card shoes={shoes[0]} />        
+            <Card shoes={shoes[1]} />        
+            <Card shoes={shoes[2]} />    */}
+            {              
+              shoes.map((shoe,i)=>{
+                return <Card shoes={shoes[i]} i={i+1} key={shoe.id} />        
+              // return <Card shoes={shoe} />        
+              })
+            }     
         </div>
       </div>
       <Container>
-      <Row>
-        <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
-      </Row>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row>
-    </Container>
-   </div>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+    </div>
   );
+}
+
+function Card(props) { 
+ return(
+  <div className="col-md-4">
+    <img src={'shoes'+props.i+'.jpg'} width="100%" />
+    <h4>{props.shoes.title}</h4>
+    <p>
+      {props.shoes.content} & {props.shoes.price}
+    </p>
+  </div>
+  )
 }
 
 export default App;
