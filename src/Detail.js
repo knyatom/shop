e276e7beaa9ded8ebaa8ab4aad103eee3ed7c173
kny <knyatom@daum.net>
2021-09-10 00:26:18 +0900

    910애니메이션

--- src/Detail.js
+++ src/Detail.js
@@ -1,7 +1,11 @@
-import React, { useState, useEffect,useContext  } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import styled from "styled-components";
-import {재고context} from './App.js';
+import { 재고context } from './App.js';
+import {
+  Navbar, Container, Nav, NavDropdown, Button, Row, Col,
+} from "react-bootstrap";
+import { CSSTransition } from "react-transition-group";
 
 let 박스 = styled.div`
   padding: 20px;
@@ -13,9 +17,13 @@ let 제목 = styled.h4`
 `;
 
 function Detail(props) {
+
   let [alert, alert변경] = useState(true);
-  let [inputData, inputData변경]=useState('');
-  let 재고=useContext(재고context);
+  let [inputData, inputData변경] = useState('');
+  let 재고 = useContext(재고context);
+
+  let [누른탭, 누른탭변경] = useState(0);
+  let [스위치, 스위치변경]=useState(false);
 
   // 업데이트될때 항상 실행된다.
   useEffect(() => {
@@ -25,10 +33,10 @@ function Detail(props) {
     }, 3000);
     console.log('welcome')
     //return function 다음실행() { }
-    return ()=>{ clearTimeout(타이머)}
-  },[]);
+    return () => { clearTimeout(타이머) }
+  }, []);
   // alert라는 state가 변경될때만 실행된다.
-   // 조건기능 한번만 실행하고 끝낸다.
+  // 조건기능 한번만 실행하고 끝낸다.
 
   let history = useHistory();
   let { id } = useParams();
@@ -43,7 +51,7 @@ function Detail(props) {
         <제목 색상={"red"} className="red"> Detail(상세페이지) </제목>
         {/* <제목 색상={'blue'}> Detail(상세페이지) </제목> */}
       </박스>
-      <input onChange={(e)=>{inputData변경(e.target.value)}}/>
+      <input onChange={(e) => { inputData변경(e.target.value) }} />
 
       {alert === true ? (<div className="my-alert2">
         <p>재고가 얼마 남지 않았습니다.</p>
@@ -64,9 +72,9 @@ function Detail(props) {
           <p>{찾은상품.price}원</p>
 
           <Info 재고={props.재고} ></Info>
-          
-          <button className="btn btn-danger" onClick={()=>{
-            props.재고변경([9,10,12])
+
+          <button className="btn btn-danger" onClick={() => {
+            props.재고변경([9, 10, 12])
           }}>주문하기</button>
 
           <button
@@ -84,16 +92,56 @@ function Detail(props) {
               history.push("/");
             }}
           >
-            홈으로가기 
+            홈으로가기
           </button>{재고}
         </div>
       </div>
+
+      <Nav className="mt-5" variant="tabs" defaultActiveKey="link-0">
+        <Nav.Item>
+          <Nav.Link eventKey="link-0" onClick={() => { 스위치변경(false);누른탭변경(0) }}>Tab1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1" onClick={() => { 스위치변경(false);누른탭변경(1) }}>Tab2</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2" onClick={() => { 스위치변경(false);누른탭변경(2) }}>Tab3   </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <CSSTransition in={스위치} classNames="wow" timeout={500}>
+        <TabContent 누른탭={누른탭} 스위치변경={스위치변경}/>
+      </CSSTransition>
+
     </div>
   );
 }
 
-function Info(props){
-  return(
+function TabContent(props) {
+
+  useEffect(()=>{
+    props.스위치변경(true);
+  })
+
+  if (props.누른탭 === 0) {
+    return <div>0번째 내용입니다.</div>
+  } else if (props.누른탭 === 1) {
+    return <div>1번째 내용입니다.</div>
+  } else {
+    return <div>2번째 내용입니다.</div>
+  }
+}
+// function TabContent({누른탭}) {
+//   if (누른탭 === 0) {
+//     return <div>0번째 내용입니다.</div>
+//   } else if (누른탭 === 1) {
+//     return <div>1번째 내용입니다.</div>
+//   } else {
+//     return <div>2번째 내용입니다.</div>
+//   }
+// }
+
+function Info(props) {
+  return (
     <p>재고: {props.재고} </p>
   );
 }
