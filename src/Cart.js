5130b1fe9f790ceada85a9439433e71a30d70fe4
kny <knyatom@daum.net>
2021-09-13 01:00:20 +0900

    913

--- src/Cart.js
+++ src/Cart.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Table} from 'react-bootstrap';
-import {connect} from 'react-redux';
+import { Table } from 'react-bootstrap';
+import { connect } from 'react-redux';
 
 function Cart(props) {
     return (
@@ -12,37 +12,45 @@ function Cart(props) {
                             <th>#</th>
                             <th>상품명</th>
                             <th>수량</th>
-                            <th>변경</th>                            
+                            <th>변경</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            props.state.map((a,i)=>{
-                                return(
+                            props.state.map((a, i) => {
+                                return (
                                     <tr key={i}>
                                         <td>{a.id}</td>
                                         <td>{a.name}</td>
                                         <td>{a.quan}</td>
                                         <td>
-                                            <button onClick={()=>{props.dispatch({type:"수량증가"})}}>+</button>
-                                            <button onClick={()=>{props.dispatch({type:"수량감소"})}}>-</button>
+                                            <button onClick={() => { props.dispatch({ type: "수량증가" }) }}>+</button>
+                                            <button onClick={() => { props.dispatch({ type: "수량감소" }) }}>-</button>
                                         </td>
                                     </tr>
                                 )
                             })
                         }
-                        
+
                     </tbody>
-                </Table>      
+                </Table>
+                {props.alert열렸니 === true ? (
+                    <div class="my-alert2">
+                        <p>지금 구매하시면 신규할인 20% </p>
+                        <button onClick={() => { props.dispatch({ type: "alert닫기" }) }}>닫기</button>
+                    </div>) : null
+                }
             </div>
         </div>
     )
 }
 
-function state를props화(state){
-    return{ 
+function state를props화(state) {
+    console.log(state);
+    return {
         //상품명:state[0].name
-        state:state
+        state: state.reducer,
+        alert열렸니: state.reducer2
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -7,36 +7,40 @@ import App from './App';
 //import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
+
+let alert초기값=true;
+
+function reducer2(state=alert초기값,액션){  
+  if(액션.type==='alert닫기'){
+    state=false;
+    return state;
+  }else{
+    return state;
+  }  
+}
 
 let 초기값 = [
   { id: 0, name: '멋진신발', quan: 2 },
   { id: 1, name: '멋진신발2', quan: 4 }
 ];
 
-function reducer(state=초기값,액션) {
-  if(액션.type==='수량증가'){
-    let copy=[...state];
+function reducer(state = 초기값, 액션) {
+  if (액션.type === '수량증가') {
+    let copy = [...state];
     copy[0].quan++;
     return copy;
-  }else if(액션.type==="수량감소"){
-    let copy=[...state];
-    copy[0].quan=copy[0].quan-1;
-
-    if(copy[0].quan<0) {copy[0].quan=0;}
-    
+  } else if (액션.type === "수량감소") {
+    let copy = [...state];
+    copy[0].quan = copy[0].quan - 1;
+    if (copy[0].quan < 0) { copy[0].quan = 0; }
     return copy;    
-  }else{
+  } else {
     return state;
-  }  
+  }
 }
 
-let store = createStore(reducer);
-
-
-
-
-
+let store = createStore(combineReducers({reducer,reducer2}));
 
 ReactDOM.render(
   <React.StrictMode>
